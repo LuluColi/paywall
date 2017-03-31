3b2afeec6c30ad1ffb2aee32477485ef39a65c75
Lulu Yang <luluyang@Lulus-rhythm.local>
2017-03-31 18:25:49 +0800

    update activity img

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const gulp = require('gulp');
+
+const ROOT_PATH = path.resolve(__dirname);
+
+// Deploy
+const ghPages = require('gulp-gh-pages');
+const rsync = require('gulp-rsync');
+const PUBLIC_DIR = path.resolve(ROOT_PATH, 'build/**/*');
+
+gulp.task('deploy:prod', () => (
+  gulp.src(PUBLIC_DIR).pipe(ghPages({
+    remoteUrl: 'ssh://git@github.com/LuluColi/paywall.git',
+  }))
+));
+
+gulp.task('deploy:staging', () => {
+  return gulp.src(PUBLIC_DIR)
+    .pipe(rsync({
+      root: 'build/',
+      hostname: 'showcase.initiumlab.com',
+      username: 'vagrant',
+      destination: `/home/vagrant/web/paywall`,
+      incremental: true,
+      recursive: true,
+      clean: true,
+      emptyDirectories: true,
+    }));
+});
